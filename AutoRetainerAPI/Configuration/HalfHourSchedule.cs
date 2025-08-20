73cf745bf1bb371dab929830006e882f029b4b46
Limiana <5073202+Limiana@users.noreply.github.com>
2025-08-20 09:55:41 +0300

    Update api

---
+++ AutoRetainerAPI/Configuration/HalfHourSchedule.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using ECommons.Logging;
+
+namespace AutoRetainerAPI.Configuration;
+[Serializable]
+public class HalfHourSchedule
+{
+    public Dictionary<int, IntervalState> Intervals = [];
+
+    public void SetState(int intervalIndex, IntervalState state)
+    {
+        if(!IsValidIndex(intervalIndex))
+        {
+            PluginLog.Error($"Invalid interval index: {intervalIndex}");
+            return;
+        }
+
+        if(state == IntervalState.Active)
+        {
+            Intervals.Remove(intervalIndex);
+        }
+        else
+        {
+            Intervals[intervalIndex] = state;
+        }
+    }
+
+    public IntervalState GetState(int intervalIndex)
+    {
+        if(!IsValidIndex(intervalIndex))
+        {
+            PluginLog.Error($"Invalid interval index: {intervalIndex}");
+            return IntervalState.Inactive;
+        }
+
+        if(Intervals.TryGetValue(intervalIndex, out IntervalState state))
+        {
+            return state;
+        }
+        return IntervalState.Active;
+    }
+
+    public IntervalState GetCurrentState()
+    {
+        return GetStateAt(DateTime.Now);
+    }
+
+    public IntervalState GetStateAt(DateTime time)
+    {
+        int index = GetIntervalIndex(time);
+        return GetState(index);
+    }
+
+    private static int GetIntervalIndex(DateTime time)
+    {
+        int totalMinutes = time.Hour * 60 + time.Minute;
+        return totalMinutes / 30;
+    }
+
+    private static bool IsValidIndex(int index)
+    {
+        return index >= 0 && index < 48;
+    }
+}
